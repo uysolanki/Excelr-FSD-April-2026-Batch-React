82220552dc907741d034cdc4a7c78a9ee2d3c65d
uysolanki <uysolanki@gmail.com>
2026-04-27 18:15:00 +0530

    java script day 1

---
+++ day1/script1.js
@@ -0,0 +1,39 @@
+// const divElement=document.getElementById("one")
+// divElement.innerHTML="Welcome to FSD"
+
+// let player={
+//     jno:18,
+//     pname:"Virat",
+//     mp:100,
+//     rs:5000,
+//     'mom-name':'Saroj',
+//     dad:100,
+//     'dad-name':'Prem Nath'
+// }
+
+// const name=99
+
+// console.log('I like ' , player.pname);
+// console.log(`I like ${player.pname}`)
+// console.log(player['pname'])
+
+// console.log(player['mom-name'])
+// console.log(player.dad-name)
+// console.log(player['dad-name'])
+// console.log(player)
+// player.centuries=85
+
+
+let player={
+    pno:18,
+    pname:'Virat'
+}
+console.log(player)
+player.pno=45;
+player.pname='Rohit';
+console.log(player)
+
+
+player="Apple"
+
+
