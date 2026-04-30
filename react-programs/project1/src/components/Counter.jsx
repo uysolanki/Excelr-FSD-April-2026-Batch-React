a078191fda2de4db8b54e2699220960a9fd645f8
uysolanki <uysolanki@gmail.com>
2026-04-30 18:55:45 +0530

    counter component using arrow

--- react-programs/project1/src/components/Counter.jsx
+++ react-programs/project1/src/components/Counter.jsx
@@ -4,29 +4,28 @@ import { useState } from 'react'
 const Counter = () => {
     //let counter=0
     //whatever prefix "use.." is known as hooks in react
-    let [counter,setCounter]=useState(0)    //initial counter
-    function increment()
-    {
-        if(counter<=9)
-        setCounter(++counter)
+    let [counter, setCounter] = useState(0)    //initial counter
+    function increment() {
+        if (counter <= 9)
+            setCounter(++counter)
         else
-        alert('Limit Reached')
+            alert('Limit Reached')
     }
 
-    function decrement()
-    {
-        if(counter>0)
-        setCounter(--counter)
-        else
-        alert('Limit Reached')
-    }
-  return (
-    <div>
-        <button onClick={increment}>My Increment</button>
-        <button onClick={decrement}>My Decrement</button>
-        <span>{counter}</span>
-    </div>
-  )
+    return (
+        <div>
+            <button onClick={increment}>My Increment</button>
+            <button onClick={() => {
+                if (counter > 0)
+                    setCounter(--counter)
+                else
+                    alert('Limit Reached')
+            }}>My Decrement</button>
+            <button>My Increment by 2</button>
+            <button>My Decrement by 2</button>
+            <span>{counter}</span>
+        </div>
+    )
 }
 
 export default Counter
