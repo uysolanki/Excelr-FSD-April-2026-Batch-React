f919d338895181a2c5039c1ef3bffd0841c876d3
uysolanki <uysolanki@gmail.com>
2026-04-30 17:54:50 +0530

    react day 1

---
+++ react-programs/project1/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  //pure JS code
+  return (
+    <div>
+        <ol className="menubar">
+          <li>File</li>
+          <li>Edit</li>
+          <li>Selection</li>
+          <li>View</li>
+        </ol>
+    </div>
+  )
+}
+
+export default App
