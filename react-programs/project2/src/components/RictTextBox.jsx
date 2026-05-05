dad78597c42823271040005451a5d6c8a1841712
uysolanki <uysolanki@gmail.com>
2026-05-05 18:01:16 +0530

    text area project

--- react-programs/project2/src/components/RictTextBox.jsx
+++ react-programs/project2/src/components/RictTextBox.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import './RictTextBox.css'
+import { CgFormatUppercase } from "react-icons/cg";
+import { RxLetterCaseLowercase } from "react-icons/rx";
+import { GrClearOption } from "react-icons/gr";
 
 const RictTextBox = () => {
 
@@ -30,14 +33,14 @@ const RictTextBox = () => {
             <h3>Preview</h3>
             <p>{previewText}</p>
             <div>   
-                <button onClick={convertToUpper}>Upper Case</button>
-                <button onClick={()=>setPreviewText(text.toLowerCase())}>Lower Case</button>
-                <button onClick={clearData}>Clear</button>
+                <button onClick={convertToUpper}>Upper Case <CgFormatUppercase /></button>
+                <button onClick={()=>setPreviewText(text.toLowerCase())}>Lower Case <RxLetterCaseLowercase /></button>
+                <button onClick={clearData}>Clear <GrClearOption /></button>
             </div>
             <div>
                 <p>Character Count {text?<span>{text.length}</span>:<span>0</span>}</p>
                 <p>Word Count {text?<span>{text.split(" ").length}</span>:<span>0</span>}</p>
-                <p>Reading Time {text.split(" ").length/200} minutes</p>
+                <p>Reading Time {text? <span>{text.split(" ").length/200}</span> : 0} minutes</p>
             </div>
         </div>
     </div>
