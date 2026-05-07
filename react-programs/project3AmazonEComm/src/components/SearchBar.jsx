fbba5233f144cec2e5d85d21ad5f571a7c24cce0
uysolanki <uysolanki@gmail.com>
2026-05-07 17:25:36 +0530

    search bar

---
+++ react-programs/project3AmazonEComm/src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SearchBar.css'
+
+const SearchBar = ({handleChange}) => {
+  return (
+    <>
+    <input type="text" onChange={handleChange}/>
+    </>
+  )
+}
+
+export default SearchBar
--- react-programs/project3AmazonEComm/src/components/ShowAllProducts3.jsx
+++ react-programs/project3AmazonEComm/src/components/ShowAllProducts3.jsx
@@ -8,6 +8,7 @@ import { useState } from 'react';
 import FilterByCategoryBar from './FilterByCategoryBar';
 import FilterByPrice from './FilterByPrice';
 import FilterByPrice2 from './FilterByPrice2';
+import SearchBar from './SearchBar';
 
 const ShowAllProducts3 = () => {
 
@@ -62,11 +63,23 @@ const ShowAllProducts3 = () => {
 
         setProducts(filteredProducts)
     }
+
+    function searchProduct(event)
+    {
+        let searchText=event.target.value;
+        console.log(searchText)
+
+        const searchedProducts=buproducts.filter(
+            (prod)=> (prod.title.toLowerCase().includes(searchText.toLowerCase())) || prod.description.toLowerCase().includes(searchText.toLowerCase()))
+                       
+        setProducts(searchedProducts)
+    }
     return (
         <>
         <FilterByCategoryBar categories={allCategories} handleClick={filterByCategory}/>
         {/* <FilterByPrice handleClick={filterByPrice}/> */}
         <FilterByPrice2 handleChange={filterByPriceNew}/>
+        <SearchBar handleChange={searchProduct}/>
         <div className="container py-5">
             <div className="row g-4">
                 {products.map((product, index) => {
